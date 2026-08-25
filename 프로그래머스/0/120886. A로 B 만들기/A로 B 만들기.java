e03f9d0859b92292d85ca86201a6ad48170059e3
siyun <si8026800@gmail.com>
2026-08-25 23:42:54 +0900

    [level 0] Title: A로 B 만들기, Time: 0.65 ms, Memory: 82.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(String before, String after) {
+        char[] beforeArr = before.toCharArray();
+        char[] afterArr = after.toCharArray();
+        
+        Arrays.sort(beforeArr);
+        Arrays.sort(afterArr);
+        
+        if (Arrays.equals(beforeArr, afterArr)) {
+            return 1;
+        }
+        return 0;
+    }
+}
