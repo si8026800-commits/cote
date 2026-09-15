e713f8e02810079c990f95422d40972df43d11b6
siyun <si8026800@gmail.com>
2026-09-15 20:30:00 +0900

    [level 0] Title: 글자 지우기, Time: 0.06 ms, Memory: 72.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String solution(String my_string, int[] indices) {
+        boolean[] remove = new boolean[my_string.length()];
+        
+        for(int idx : indices){
+            remove[idx] = true;
+        }
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i = 0; i < my_string.length(); i++) {
+            if(!remove[i]) {
+                sb.append(my_string.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
+}
